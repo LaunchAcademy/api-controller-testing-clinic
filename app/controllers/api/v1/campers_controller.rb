37833023ca4d9a61addb30bcf29d5eaf9cec393b
dovakeen118 <kegillis118@gmail.com>
2020-04-22 12:30:52 -0400

    final code

---
+++ app/controllers/api/v1/campers_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::CampersController < ApplicationController
+  def index
+    render json: Camper.all
+  end
+
+  def create
+    camper = Camper.new(camper_params)
+
+    if camper.save
+      render json: camper
+    else
+      render json: { errors: "Camper did not save" }
+      # render json: { errors: camper.errors.full_messages }
+    end
+  end
+
+  private
+
+  def camper_params
+    params.require(:camper).permit(:name, :campsite_id)
+  end
+end
